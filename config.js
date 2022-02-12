cd48383c3865fefd1e8b66ff0d04377597a04b99
Marwan361 <60656147+Marwan361@users.noreply.github.com>
2022-02-12 18:37:56 +0200

    Update and rename config-example.js to config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "",
+  token: "OTEzOTA0MjUxNjc4MzcxODcw.YaFRqQ._8JdV33YEojTvOOZSktsKXO2oIA",
   inv:
     "https://discordapp.com/oauth2/authorize?&client_id=[CLIENT-ID-HERE]&scope=bot&permissions=1073768448",
   prefix: ">"
